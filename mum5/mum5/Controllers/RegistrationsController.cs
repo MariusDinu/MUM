bdb10f8b40baa237fa64747f2da014f742c65603
Marius <mariusd30@yahoo.com>
2020-03-06 16:51:42 +0200

    add stuff

---
+++ mum5/mum5/Controllers/RegistrationsController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using mum5.Models;
+
+namespace mum5.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        private readonly RegistrationContext _context;
+
+        public RegistrationsController(RegistrationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Registrations
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Registration.ToListAsync());
+        }
+
+     
+
+        // GET: Registrations/Create
+        public IActionResult AddOrEdit(int id=0)
+        {
+            return View(new Registration());
+        }
+
+        // POST: Registrations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("UserID,Username,Password,Email")] Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(registration);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(registration);
+        }
+
+        // GET: Registrations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var registration = await _context.Registration
+                .FirstOrDefaultAsync(m => m.UserID == id);
+            if (registration == null)
+            {
+                return NotFound();
+            }
+
+            return View(registration);
+        }
+
+        
+
+        private bool RegistrationExists(int id)
+        {
+            return _context.Registration.Any(e => e.UserID == id);
+        }
+    }
+}
---
+++ mum5/mum5/Models/Registration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mum5.Models
+{   
+    public class Registration
+    {
+        [Key]
+        public int UserID { get; set; }
+        [Column(TypeName ="varchar(30)")]
+        public string Email { get; set; }
+        [Column(TypeName = "varchar(20)")]
+        [Required]
+      public string Username { get; set; }
+        [Column(TypeName = "varchar(15)")]
+        [Required]
+        public string Password { get; set; }
+        
+    }
+}
