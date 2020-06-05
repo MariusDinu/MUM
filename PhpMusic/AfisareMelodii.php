78c19f55a7c3c9b6980618b6d5d7eec08f35aa8d
Marius <mariusd30@yahoo.com>
2020-06-05 17:35:37 +0300

    change css for app
    add granit and fire + edit show music

--- PhpMusic/AfisareMelodii.php
+++ PhpMusic/AfisareMelodii.php
@@ -24,8 +24,6 @@ function afisareUser(){
      
     
       echo   "<div class='"."Melodie"."' id='".$row['Id']."'>".
-      "<div class='"."first"."'>".
-      "<button onclick='playMusic(this)' id='".$row['Local']."' title='"."Play"."' class='Play b8 b9 ba ao bb bc bd be bf bg bh bi bj'><svg viewBox='"."0 0 26 26"."' xmlns='"."http://www.w3.org/2000/svg"."' style='"."width: 100%; height: 100%; "."'><title>Play</title><path d='"."M7.712 22.04a.732.732 0 0 1-.806.007.767.767 0 0 1-.406-.703V4.656c0-.31.135-.544.406-.703.271-.16.54-.157.806.006l14.458 8.332c.266.163.4.4.4.709 0 .31-.134.546-.4.71L7.712 22.04z"."' fill='"."currentColor"."' fill-rule='"."evenodd"."'></path></svg></button></div>".
       "<a class='"."form-inline"."' id='".$row['Id']."' onclick='AfisareMelodie(this)'>".
       "<div class='second' id='".$row['Id']."' value='".$row['Id']."'>".
       "<table style='"."width:100%"."'>".
@@ -33,7 +31,9 @@ function afisareUser(){
       "<tr><th id='Artist:".$row['Artist']."'>Artist:".$row['Artist']."</th></tr>".
       "<tr><th id='Gen:".$row['Gen']."'>Gen: ".$row['Gen']."</th></tr>".
       "<tr><th id='Album:".$row['Album']."'>Album: ".$row['Album']."</th></tr>"
-     ."</table></div></a>";
+     ."</table></div></a> <div class='"."first"."'>".
+     "<button onclick='playMusic(this)' id='".$row['Local']."' title='"."Play"."' class='Play b8 b9 ba ao bb bc bd be bf bg bh bi bj'><svg viewBox='"."0 0 26 26"."' xmlns='"."http://www.w3.org/2000/svg"."' style='"."width: 100%; height: 100%; "."'><title>Play</title><path d='"."M7.712 22.04a.732.732 0 0 1-.806.007.767.767 0 0 1-.406-.703V4.656c0-.31.135-.544.406-.703.271-.16.54-.157.806.006l14.458 8.332c.266.163.4.4.4.709 0 .31-.134.546-.4.71L7.712 22.04z"."' fill='"."currentColor"."' fill-rule='"."evenodd"."'></path></svg></button></div>"
+     ;
      if(verifyButton($row['Name'])==1)
      {
        echo  "<button class='"."buttonFavoritAdd"."'"." id='".$row['Id']."' data-toggle='"."modal"."' data-target='"."#room-settings"."' onclick='afisarefav(this)' value='".$row['Id']."'>Adaugare la favorite</button> ";
@@ -57,12 +57,8 @@ function afisareAdmin(){
       
         echo 
         "<div class='"."Melodie"."' id='".$row['Id']."'>".
-        "<div class='"."first"."'>".
-        
-       "<button class='"."buttonAdmin"."'"." id='".$row['Id']."' data-toggle='"."modal"."' data-target='"."#room-settings"."' onclick='modifyMelodie(this)' value='".$row['Id']."'>Modify</button> ".
-       "<button class='"."buttonAdmin"."'"." id='".$row['Id']."' onclick='deleteMelodie(this)' value='".$row['Id']."'>Delete</button> ".
        
-        "<button onclick='playMusic(this)' id='".$row['Local']."' title='"."Play"."' class='Play b8 b9 ba ao bb bc bd be bf bg bh bi bj'><svg viewBox='"."0 0 26 26"."' xmlns='"."http://www.w3.org/2000/svg"."' style='"."width: 100%; height: 100%; "."'><title>Play</title><path d='"."M7.712 22.04a.732.732 0 0 1-.806.007.767.767 0 0 1-.406-.703V4.656c0-.31.135-.544.406-.703.271-.16.54-.157.806.006l14.458 8.332c.266.163.4.4.4.709 0 .31-.134.546-.4.71L7.712 22.04z"."' fill='"."currentColor"."' fill-rule='"."evenodd"."'></path></svg></button></div>".
+       
         "<a class='"."form-inline"."' href='/MUM/Pages/music=?id=".$row['Id']."&object=".$row['Name']."'>".
         "<div class='second' id='Id' value='".$row['Id']."'>".
         "<table style='"."width:100%"."'>".
@@ -71,7 +67,12 @@ function afisareAdmin(){
         "<tr> <th id='Gen:".$row['Gen']."'>Gen: ".$row['Gen']."</th></tr>".
         "<tr> <th id='Album:".$row['Album']."'>Album: ".$row['Album']."</th></tr>"
        ."</table></div></a>".
+       "<div class='"."first"."'>".
+        
+       "<button class='"."buttonAdmin"."'"." id='".$row['Id']."' data-toggle='"."modal"."' data-target='"."#room-settings"."' onclick='modifyMelodie(this)' value='".$row['Id']."'>Modify</button> ".
+       "<button class='"."buttonAdmin"."'"." id='".$row['Id']."' onclick='deleteMelodie(this)' value='".$row['Id']."'>Delete</button>".
        
+       "<button onclick='playMusic(this)' id='".$row['Local']."' title='"."Play"."' class='Play b8 b9 ba ao bb bc bd be bf bg bh bi bj'><svg viewBox='"."0 0 26 26"."' xmlns='"."http://www.w3.org/2000/svg"."' style='"."width: 100%; height: 100%; "."'><title>Play</title><path d='"."M7.712 22.04a.732.732 0 0 1-.806.007.767.767 0 0 1-.406-.703V4.656c0-.31.135-.544.406-.703.271-.16.54-.157.806.006l14.458 8.332c.266.163.4.4.4.709 0 .31-.134.546-.4.71L7.712 22.04z"."' fill='"."currentColor"."' fill-rule='"."evenodd"."'></path></svg></button></div>".
        
        "</div> <br></br>";
        
