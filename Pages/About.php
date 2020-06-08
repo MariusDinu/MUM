280ae4c53eb7d080563a893d8950d267401b806a
BogdanZaharia98 <48146977+BogdanZaharia98@users.noreply.github.com>
2020-06-08 14:54:46 +0300

    add About.php , empty css

---
+++ Pages/About.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width">
+<title> Scholarity HTML </title>
+    <link href="/MUM/Css/About.css" rel="stylesheet">
+</head>
+
+
+<body vocab="http://schema.org/"  xmlns:xsd="http://www.w3.org/2001/XMLSchema#" xmlns:sa="https://ns.science.ai/">
+
+
+
+
+<article resource="#" typeof="schema:ScholarlyArticle">
+
+<h1>About Page</h1>
+  <section>
+  <h2>Contributors List</h2>
+    <ol>
+      <!-- Zaharia Bogdan -->
+      <li property="schema:author" typeof="sa:ContributorRole">
+        <a property="schema:author" href="#" typeof="schema:Person">
+          <span property="schema:givenName">Bogdan</span>
+          <span property="schema:familyName">Zaharia</span>
+        </a>
+        <a href="#scienceai" property="sa:roleAffiliation" resource="https://ns.science.ai/">a</a>
+        <sup property="sa:roleContactPoint" typeof="schema:ContactPoint">
+          <a property="schema:email" href="mailto:bogdan.zaharia98@gmail.com" title="contributor">✉</a>
+        </sup>
+      </li>
+      <!--Miron Dorin-->
+      <li property="schema:author" typeof="sa:ContributorRole">
+        <a property="schema:author" href="#" typeof="schema:Person">
+          <span property="schema:givenName">Dorin</span>
+          <span property="schema:familyName">Miron</span>
+        </a>
+        <a href="#scienceai" property="sa:roleAffiliation" resource="https://ns.science.ai/">a</a>
+      </li>
+    </ol>
+
+
+
+
+
+
+</body>
+
+
+
+
+</html>
